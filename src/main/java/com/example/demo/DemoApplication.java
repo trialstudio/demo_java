3af722dc1b3cbc0af52bbef806bc7af4153f7b0f
ibacangan <i.bacangan@gmail.com>
2022-03-21 13:00:20 +0800

    add test rest endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@RestController
+class Test {
+
+	@GetMapping
+	public String test() {
+		return "test";
+	}
 }
